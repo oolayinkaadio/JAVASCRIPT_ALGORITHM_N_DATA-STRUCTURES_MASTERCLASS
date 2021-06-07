f1c8c2c4585887e969d1fc75e104336689e5ddee
Oloaosebikan Olayinka <oolayinkaadio2gmail.com>
2021-06-07 17:16:29 +0100

    null

---
+++ Analyzing_Performance_of_Arrays_and_Objects/index.js
@@ -0,0 +1,16 @@
+// The Big O of Objects
+
+let instructor = {
+    firstName: 'Yinka',
+    isInstructor: true,
+    favoriteNumbers: [1, 2, 3, 4]
+};
+
+console.log(Object.keys(instructor));
+
+console.log(instructor.hasOwnProperty('firstName'));
+
+
+// ARRAYS
+let names = ["Tola", "Shola", "Toyin"];
+
